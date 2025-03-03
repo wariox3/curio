ed1fb75bd13726f127dd4cef146d5414f959c498
afcanop <afcanop@gmail.com>
2025-03-03 11:44:15 -0500

    feat: consultar cantidad al inicializar FacturaItemsCardComponent

--- src/app/pages/facturacion/componentes/factura-items-card/factura-items-card.component.ts
+++ src/app/pages/facturacion/componentes/factura-items-card/factura-items-card.component.ts
@@ -1,6 +1,6 @@
 import { obtenerItemCantidadFacturaActiva } from './../../../../redux/selectors/factura.selectors';
 import { NgClass } from '@angular/common';
-import { Component, inject,  Input, signal } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import { Item } from '@interfaces/item.interface';
 import { FacturaReduxService } from '../../services/factura-redux.service';
 
@@ -10,32 +10,33 @@ import { FacturaReduxService } from '../../services/factura-redux.service';
   imports: [NgClass],
   templateUrl: './factura-items-card.component.html',
 })
-export class FacturaItemsCardComponent {
+export class FacturaItemsCardComponent implements OnInit {
   private _facturaReduxService = inject(FacturaReduxService);
   public cantidadSignal = signal(0);
 
   @Input() item: Item;
 
+  ngOnInit(): void {
+    this._itemCantidad(this.item.id);
+  }
 
   seleccionarProducto(item: Item) {
-
     if (this.cantidadSignal() === 0) {
       this._agregarProductoAFactura(item);
       this._itemCantidad(item.id);
     } else {
-      this._agregarNuevaCantidad(item)
+      this._agregarNuevaCantidad(item);
       this._itemCantidad(item.id);
     }
     this._facturaReduxService.calcularSubtotal(item.id);
   }
 
-  private _agregarNuevaCantidad(item: Item){
+  private _agregarNuevaCantidad(item: Item) {
     this._facturaReduxService.actualizarCantidadItem(
       item.id,
       (item.cantidad += 1)
     );
   }
-
 
   private _agregarProductoAFactura(item: Item) {
     this._facturaReduxService.agregarItem(item);
@@ -45,9 +46,9 @@ export class FacturaItemsCardComponent {
     this._facturaReduxService
       .obtenerItemCantidad(itemId)
       .subscribe((cantidad) => {
-        this.cantidadSignal.set(cantidad)
-        if(this.cantidadSignal() === 0){
-          this.cantidadSignal.set(0)
+        this.cantidadSignal.set(cantidad);
+        if (this.cantidadSignal() === 0) {
+          this.cantidadSignal.set(0);
         }
       });
   }
