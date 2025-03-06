7a373891b65d1d77811bd66cad786ebba760274d
afcanop <afcanop@gmail.com>
2025-03-06 11:08:40 -0500

    feat: retirar factura al terminar el pago

--- src/app/pages/facturacion/componentes/factura-medios-pago/factura-medios-exitoso/factura-medios-exitoso.component.ts
+++ src/app/pages/facturacion/componentes/factura-medios-pago/factura-medios-exitoso/factura-medios-exitoso.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { KTModal } from '@metronic/components/modal/modal';
+import { FacturaReduxService } from '../../../services/factura-redux.service';
 
 @Component({
   selector: 'app-factura-medios-exitoso',
@@ -8,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './factura-medios-exitoso.component.scss'
 })
 export class FacturaMediosExitosoComponent {
+  private _facturaReduxService = inject(FacturaReduxService);
+  public tabActivo = this._facturaReduxService.facturaTabActivo;
+
+  gestionNuevaFactura(){
+    this._facturaReduxService.retirarFactura(this.tabActivo());
+  }
 
 }
