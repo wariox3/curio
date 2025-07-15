7b96de93aec510340265eee0a3a58c6d311fcf23
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-15 10:43:44 -0500

    feat: acceso restringido

--- src/app/pages/contenedores/componentes/contenedor-lista/contenedor-lista.component.ts
+++ src/app/pages/contenedores/componentes/contenedor-lista/contenedor-lista.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '@componentes/ui/button/button.component';
 import {
@@ -6,8 +6,8 @@ import {
   ListaContenedoresRespuesta,
 } from '@interfaces/contenedores.interface';
 import { Store } from '@ngrx/store';
-import { obtenerUsuarioId } from '@redux/selectors/usuario.selectors';
-import { catchError, of, switchMap, tap } from 'rxjs';
+import { obtenerUsuarioFechaLimitePago, obtenerUsuarioId, obtenerUsuarioSocio, obtenerUsuarioVrSaldo } from '@redux/selectors/usuario.selectors';
+import { catchError, combineLatest, of, Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { AlertaService } from 'src/app/shared/services/alerta.service';
 import { ContenedorApiService } from '../../services/contenedor-api.service';
 import { ContenedorReduxService } from '../../../../redux/services/contenedor-redux.service';
@@ -20,22 +20,27 @@ import { ConfiguracionReduxService } from '@redux/services/configuracion-redux.s
   imports: [ButtonComponent],
   templateUrl: './contenedor-lista.component.html',
 })
-export default class ContenedorListaComponent implements OnInit {
+export default class ContenedorListaComponent implements OnInit, OnDestroy {
   private _contenedorService = inject(ContenedorApiService);
-  private _configuracionGeneralApiService = inject(
-    ConfiguracionGeneralApiService,
-  );
   private _contenedorReduxService = inject(ContenedorReduxService);
-  private _ConfiguracionReduxService = inject(
-    ConfiguracionReduxService,
-  );
   private _store = inject(Store);
   private _alertaService = inject(AlertaService);
   private _router = inject(Router);
-  public arrConectando: boolean[] = [];
-  public arrContenedores = signal<any[]>([]);
-  public filtroNombre = signal<string>('');
+  private destroy$ = new Subject<void>();
+  private _configuracionGeneralApiService = inject(
+    ConfiguracionGeneralApiService,
+  );
+  private _ConfiguracionReduxService = inject(
+    ConfiguracionReduxService,
+  );
 
+  public arrConectando: boolean[] = [];
+  public fechaActual = new Date();
+  public fechaLimitePago = signal<Date>(new Date());
+  public saldo = signal<number>(0);
+  public arrContenedores = signal<any[]>([]);
+  public habilitarContenedores = signal<boolean>(true);
+  public filtroNombre = signal<string>('');
   public contenedoresFiltrados = computed(() => {
     return this.arrContenedores().filter((contenedor) =>
       contenedor.nombre
@@ -46,12 +51,35 @@ export default class ContenedorListaComponent implements OnInit {
 
   ngOnInit() {
     this.consultarLista();
+    this.x();
   }
+
+  x () {
+    combineLatest([
+      this._store.select(obtenerUsuarioFechaLimitePago),
+      this._store.select(obtenerUsuarioVrSaldo),
+    ]).subscribe(([fechaLimitePago, saldo]) => {
+      const fechaLimite =  new Date(new Date(fechaLimitePago).getTime() + 24 * 60 * 60 * 1000);
+      this.fechaLimitePago.set(fechaLimite)
+      this.saldo.set(saldo);
+
+      if (
+        this.saldo() > 0 &&
+        this.fechaLimitePago() < this.fechaActual
+      ) {
+        this.habilitarContenedores.set(false);
+      }
+
+    });
+  }
+
+
 
   consultarLista() {
     this._store
       .select(obtenerUsuarioId)
       .pipe(
+        takeUntil(this.destroy$),
         switchMap((respuestaUsuarioId) =>
           this._contenedorService.lista(respuestaUsuarioId.toString()),
         ),
@@ -72,11 +100,17 @@ export default class ContenedorListaComponent implements OnInit {
       .subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   seleccionarEmpresa(contenedor_id: string, indexContenedor: number) {
     this.arrConectando[indexContenedor] = true;
     this._contenedorService
       .detalle(contenedor_id)
       .pipe(
+        takeUntil(this.destroy$),
         tap((respuesta: ContenedorDetalle) => {
           this._contenedorReduxService.cargarContenedor(respuesta);
           this.arrConectando[indexContenedor] = false;
